215a6ec6bd5e555d60fb7e7a2cb730d51f63fb4d
danil <dany.den2014@yandex.ru>
2022-05-07 23:59:19 +0300

    добавили оставшиеся элементы в компонент продукта, и добавили Image компонент. Далее адаптив продукта

--- top-app/components/Product/Product.tsx
+++ top-app/components/Product/Product.tsx
@@ -5,47 +5,65 @@ import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
-import { priceRu } from '../../helpers/helpers';
+import { declOfNum, priceRu } from '../../helpers/helpers';
 import { Divider } from '../Divider/Divider';
+import Image from 'next/image';
 
 
 export const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
-	return (
-		<Card className={styles.product}>
-			<div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}/></div>
-			<div className={styles.title}>{product.title}</div>
-			<div className={styles.price}>
-				{priceRu(product.price)}
-				{product.oldPrice && <Tag className={styles.oldPrice} color='green' key={product.price}>{priceRu(product.price - product.oldPrice)}</Tag>}
-			</div>
-			<div className={styles.credit}>
-				{priceRu(product.credit)}/<span className={styles.month}>мес</span>
-			</div>
-			<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating}/></div>
-			<div className={styles.tags}>{product.categories.map(c => <Tag className={styles.category} key={c} color='ghost'>{c}</Tag>)}</div>
-			<div className={styles.priceTitle}>цена</div>
-			<div className={styles.creditTitle}>кредит</div>
-			<div className={styles.rateTitle}>{product.reviewCount} Отзывов</div>
-			<Divider className={styles.hr}/>
-			<div className={styles.description}>{product.description}</div>
-			<div className={styles.feature}>фичи</div>
-			<div className={styles.advBlock}>
-			{product.advantages && <div className={styles.advantages}>
-					<div className={styles.advTitle}>Преимущества</div>
-					<div >{product.advantages}</div>
-					
-				</div>}
-				{product.disadvantages &&<div className={styles.disadvantages}>
-					<div className={styles.advTitle}>Недостатки</div>
-					<div>{product.disadvantages}</div>	
-				</div>}
-			</div>
-			<Divider className={styles.hr}/>
-			<div className={styles.actions}>
-				<Button appearance='primary'>Узнать подробнее</Button>
-				<Button appearance='ghost' arrow={'right'} className={styles.reviewButton}>Читать отзывы</Button>
-			</div>
+return (
+<Card className={styles.product}>
+	<div className={styles.logo}>
+		<Image // перед этим нужно настроить next.config там нужно добавить домен с которого будут грузиться картинки
+			src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} width={70} height={70} 
+		/>
+	</div>
+	<div className={styles.title}>{product.title}</div>
+	<div className={styles.price}>
+		{priceRu(product.price)}
+		{product.oldPrice && <Tag className={styles.oldPrice} color='green' key={product.price}>{priceRu(product.price -
+			product.oldPrice)}</Tag>}
+	</div>
+	<div className={styles.credit}>
+		{priceRu(product.credit)}/<span className={styles.month}>мес</span>
+	</div>
+	<div className={styles.rating}>
+		<Rating rating={product.reviewAvg ?? product.initialRating} />
+	</div>
+	<div className={styles.tags}>{product.categories.map(c => <Tag className={styles.category} key={c} color='ghost'>{c}
+		</Tag>)}</div>
+	<div className={styles.priceTitle}>цена</div>
+	<div className={styles.creditTitle}>кредит</div>
+	<div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount,
+		['отзыв','отзыва','отзывов'])}</div>
+	<Divider className={styles.hr} />
+	<div className={styles.description}>{product.description}</div>
+	<div className={styles.feature}>
+		{product.characteristics.map(c => (
+		<div className={styles.characteristics} key={c.name}>
+			<span className={styles.characteristicsName}>{c.name}</span>
+			<span className={styles.characteristicsDots}></span>
+			<span className={styles.characteristicsValue}>{c.value}</span>
+		</div>
+		))}
+	</div>
+	<div className={styles.advBlock}>
+		{product.advantages && <div className={styles.advantages}>
+			<div className={styles.advTitle}>Преимущества</div>
+			<div>{product.advantages}</div>
 
-		</Card>
-	);
+		</div>}
+		{product.disadvantages &&<div className={styles.disadvantages}>
+			<div className={styles.advTitle}>Недостатки</div>
+			<div>{product.disadvantages}</div>
+		</div>}
+	</div>
+	<Divider className={styles.hr} />
+	<div className={styles.actions}>
+		<Button appearance='primary'>Узнать подробнее</Button>
+		<Button appearance='ghost' arrow={'right'} className={styles.reviewButton}>Читать отзывы</Button>
+	</div>
+
+</Card>
+);
 };
